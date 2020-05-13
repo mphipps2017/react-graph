89a943b2dff6c67e026114f2967e21f162b0b0ae
mphipps2017 <mnphipps98@gmail.com>
2020-05-13 16:10:53 -0500

    A simple component for interfacing with char.js

---
+++ Graph.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Line} from 'react-chartjs-2';
+
+class Graph extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            labels: this.props.xAxis,
+            datasets: [
+            {
+                label: this.props.label,
+                lineTension: 0.5,
+                backgroundColor: 'rgba(75,192,192,1)',
+                borderColor: 'rgba(0,0,0,1)',
+                borderWidth: 2,
+                data: this.props.yAxis
+            },]
+         }
+    }
+    
+    render(){
+        return(
+            <Line
+          data={this.state}
+          options={{
+            title:{
+              display:true,
+              text: this.props.title,
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
+        )
+    }
+}
+
+ Graph.defaultProps = {
+    xAxis: ['January', 'February', 'March', 'April', 'May'],
+    label: 'Rainfall',
+    yAxis: [65, 59, 80, 81, 56],
+    title: 'Average Rainfall per month',
+ }
+
+export default Graph;
